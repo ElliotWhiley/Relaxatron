32819aa1d25eef20ee2f73481bff94783ff334cc
ElliotWhiley <elliot.w@hotmail.com>
2016-01-30 13:05:25 +1300

    Added script to spawn a school of fish within a sphere - that don't spawn inside of each other  :D:D:D

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,30 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Spawner : MonoBehaviour
-{
-
-    public int numObjects = 9000;
+public class Spawner : MonoBehaviour {
+    public int worldX;
+    public int worldY;
+    public int worldZ;
+    public int numObjects;
+    public int numFishies;
     public GameObject prefab;
 
-    void Start()
-    {
-        Vector3 center = transform.position;
-        for (int i = 0; i < numObjects; i++)
-        {
-            Vector3 pos = RandomCircle(center, 5.0f);
-            Quaternion rot = Quaternion.FromToRotation(Vector3.forward, center - pos);
-            Instantiate(prefab, pos, rot);
-        }
+    void Start() {
+        SpawnSchool();
+        //Vector3 center = transform.position;
+        //for (int i = 0; i < numObjects; i++)
+        //{
+        //    Vector3 pos = RandomCircle(center, 5.0f);
+        //    Quaternion rot = Quaternion.FromToRotation(Vector3.forward, center - pos);
+        //    Instantiate(prefab, pos, rot);
+        //}
     }
 
-    Vector3 RandomCircle(Vector3 center, float radius)
-    {
+    Vector3 RandomCircle(Vector3 center, float radius) {
         float ang = Random.value * 360;
         Vector3 pos;
         pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
         pos.y = center.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
         pos.z = center.z;
         return pos;
+    }
+
+    void SpawnSchool() {
+        Vector3 spawnCentre = new Vector3(-500, 200, -500);
+        int spawnRadius = 20;
+        float spacing = 2;
+        for (int i = 0; i < numFishies; i++) {
+            // generate random pos within spawn sphere
+            Vector3 spawnPos = spawnCentre + Random.insideUnitSphere * spawnRadius;
+            // check if that pos is 'free'
+            Collider[] collisions = Physics.OverlapSphere(spawnCentre, spacing);
+            Debug.Log(collisions.Length);
+            if (collisions.Length == 1) {
+                // spawn fish at pos
+                Instantiate(prefab, spawnPos, Quaternion.Euler(new Vector3(0, 0, 0)));
+            }
+        }
     }
 }

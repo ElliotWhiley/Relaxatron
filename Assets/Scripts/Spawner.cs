04729a2de749630edb8d7cbabeb96f88d82309b6
ElliotWhiley <elliot.w@hotmail.com>
2016-01-30 19:47:00 +1300

    Refactored Spawner method

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -10,42 +10,47 @@ public class Spawner : MonoBehaviour {
     public GameObject prefab;
 
     void Start() {
-        SpawnSchool();
-        //Vector3 center = transform.position;
-        //for (int i = 0; i < numObjects; i++)
-        //{
-        //    Vector3 pos = RandomCircle(center, 5.0f);
-        //    Quaternion rot = Quaternion.FromToRotation(Vector3.forward, center - pos);
-        //    Instantiate(prefab, pos, rot);
-        //}
+        // spawn small school
+        SpawnFishies(spawnCentre: new Vector3(-400, 300, -400), spawnRadius: 50, spacing: 10, numFishies: 1000, rotation: Quaternion.Euler(new Vector3(50, 30, 10)), scale: 3f, speed: 20, forceDirection: new Vector3(50, 30, 30));
+        // spawn medium school
+        SpawnFishies(spawnCentre: new Vector3(500, 300, -200), spawnRadius: 100, spacing: 10, numFishies: 200, rotation: Quaternion.Euler(new Vector3(50, 30, 10)), scale: 10f, speed: 15, forceDirection: new Vector3(50, 30, 30));
     }
 
-    Vector3 RandomCircle(Vector3 center, float radius) {
-        float ang = Random.value * 360;
-        Vector3 pos;
-        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
-        pos.y = center.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
-        pos.z = center.z;
-        return pos;
-    }
-
-    void SpawnSchool() {
-        Vector3 spawnCentre = new Vector3(-500, 300, -500);
-        int spawnRadius = 30;
-        float spacing = 2;
+    void SpawnFishies(Vector3 spawnCentre, int spawnRadius, float spacing, int numFishies, Quaternion rotation, float scale, int speed, Vector3 forceDirection) {
         for (int i = 0; i < numFishies; i++) {
             // generate random pos within spawn sphere
             Vector3 spawnPos = spawnCentre + Random.insideUnitSphere * spawnRadius;
             // check if that pos is 'free'
             Collider[] collisions = Physics.OverlapSphere(spawnPos, spacing);
-            Debug.Log(collisions.Length);
+            if (collisions.Length == 1) {
+                // spawn fish at pos
+                GameObject fish = (GameObject)Instantiate(prefab, spawnPos, rotation);
+                // set fish size
+                fish.GetComponent<Rigidbody>().transform.localScale = new Vector3(scale, scale, scale);
+                // set fish speed
+                fish.GetComponent<Rigidbody>().AddForce(forceDirection * speed);
+            }
+        }
+    }
+
+    void SpawnLargeSchool() {
+        Vector3 spawnCentre = new Vector3(500, 300, -200);
+        int spawnRadius = 100;
+        float spacing = 10;
+        for (int i = 0; i < numFishies / 3; i++) {
+            // generate random pos within spawn sphere
+            Vector3 spawnPos = spawnCentre + Random.insideUnitSphere * spawnRadius;
+            // check if that pos is 'free'
+            Collider[] collisions = Physics.OverlapSphere(spawnPos, spacing);
             if (collisions.Length == 1) {
                 // set fish rotation
                 Quaternion rotation = Quaternion.Euler(new Vector3(50, 30, 10));
                 // spawn fish at pos
-                GameObject fish = (GameObject) Instantiate(prefab, spawnPos, rotation);
-                fish.GetComponent<Rigidbody>().AddForce(new Vector3(50, 30, 30) * 50);
-
+                GameObject fish = (GameObject)Instantiate(prefab, spawnPos, rotation);
+                // set fish size
+                fish.GetComponent<Rigidbody>().transform.localScale = new Vector3(10f, 10f, 10f);
+                // set fish speed
+                fish.GetComponent<Rigidbody>().AddForce(new Vector3(-35, 15, 20) * 15);
             }
         }
     }
